9ef0411ed9a4f98856c21312791ad16a24f5d632
Annika Santhanam <aps659@nyu.edu>
2026-02-10 16:45:29 -0500

    schematypes

--- src/sanity/schemaTypes/projects.ts
+++ src/sanity/schemaTypes/projects.ts
@@ -3,28 +3,15 @@ const projectSchema = {
     title: 'Project',
     type: 'document',
     fields: [
+      { name: 'title', title: 'Project Title', type: 'string' },
+      { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title' } },
       {
-        name: 'title',
-        title: 'Project Title',
-        type: 'string',
+        name: 'images', // Changed from mainImage
+        title: 'Project Images',
+        type: 'array',
+        of: [{ type: 'image', options: { hotspot: true } }],
       },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: { source: 'title' },
-      },
-      {
-        name: 'mainImage',
-        title: 'Background Image',
-        type: 'image',
-        options: { hotspot: true },
-      },
-      {
-          name: 'order',
-          title: 'Order',
-          type: 'number',
-      },
+      { name: 'order', title: 'Order', type: 'number' },
       {
         name: 'isProtected',
         title: 'Password Protected?',
